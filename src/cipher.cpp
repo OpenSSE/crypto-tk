d6c5d527fce6f5dc2a14aca036279d5bac7f7431
Raphael Bost <raphael_bost@alumni.brown.edu>
2015-11-21 00:36:21 +0100

    Use smaller IVs

--- src/cipher.cpp
+++ src/cipher.cpp
@@ -6,6 +6,9 @@
 
 #include <openssl/aes.h>
 
+#include <iostream>
+#include <iomanip>
+
 namespace sse
 {
 
@@ -15,7 +18,10 @@ namespace crypto
 class Cipher::CipherImpl
 {
 public:
-	static constexpr uint8_t kKeySize = 32;
+		
+	// We use 48 bits IV. 
+	// This is enough to encrypt 2^48 blocks which is the security bound of counter mode
+	static constexpr uint8_t kIVSize = 6; 
 
 	CipherImpl();
 	
@@ -35,7 +41,7 @@ public:
 private:	
 	AES_KEY aes_enc_key_;
 	
-	unsigned char iv_[AES_BLOCK_SIZE];
+	unsigned char iv_[kIVSize];
 	uint64_t remaining_block_count_;
 };
 	
@@ -84,20 +90,24 @@ Cipher::CipherImpl::~CipherImpl()
 	memset(&aes_enc_key_, 0x00, sizeof(AES_KEY));
 }
 
+#define MIN(a,b) (((a) > (b)) ? (b) : (a))
 void Cipher::CipherImpl::gen_subkeys(const unsigned char *userKey)
 {
-	if (!AES_set_encrypt_key(userKey, 128, &aes_enc_key_))
+	if (AES_set_encrypt_key(userKey, 128, &aes_enc_key_) != 0)
 	{
 		// throw an exception
 		throw std::runtime_error("Unable to init AES subkeys");
 	}
 	
-	remaining_block_count_ = 0xffffffffffffffff; // maximum value;
+	// Compute maximum number of blocks that can be encrypted with the same key
+	// This number comes from the security reduction of CTR mode (2^48 blocks at most to retain 32 bits of security)
+	// and from the IV length (not more than 2^(8*kIVSize) different IVs) 
+	remaining_block_count_ = ((uint64_t) 1) << MIN(48,8*kIVSize); 
 }
 
 void Cipher::CipherImpl::reset_iv()
 {
-	memset(iv_, 0x00, AES_BLOCK_SIZE);
+	memset(iv_, 0x00, kIVSize);
 }
 
 void Cipher::CipherImpl::encrypt(const unsigned char* in, const size_t &len, unsigned char* out)
@@ -107,15 +117,21 @@ void Cipher::CipherImpl::encrypt(const unsigned char* in, const size_t &len, uns
 		throw std::runtime_error("Too many blocks were encrypted with the same key. Encrypting using this key is now insecure.");
 	}
 	
+    unsigned char enc_iv[AES_BLOCK_SIZE];
     unsigned char ecount[AES_BLOCK_SIZE];
     memset(ecount, 0x00, AES_BLOCK_SIZE);
 	
 	unsigned int num = 0;
 	
-	memcpy(out, iv_, AES_BLOCK_SIZE); // copy iv first
+	memcpy(out, iv_, kIVSize); // copy iv first
+
+	if(kIVSize != AES_BLOCK_SIZE)
+		memset(enc_iv, 0, AES_BLOCK_SIZE);
+	
+	memcpy(enc_iv+AES_BLOCK_SIZE-kIVSize, iv_, kIVSize);
 	
 	// now append the ciphertext
-    AES_ctr128_encrypt(in, out+AES_BLOCK_SIZE, len, &aes_enc_key_, iv_, ecount, &num);
+    AES_ctr128_encrypt(in, out+kIVSize, len, &aes_enc_key_, enc_iv, ecount, &num);
 	
 	// erase ecount to avoid (partial) recovery of the last block
 	memset(ecount, 0x00, AES_BLOCK_SIZE);
@@ -124,7 +140,7 @@ void Cipher::CipherImpl::encrypt(const unsigned char* in, const size_t &len, uns
 void Cipher::CipherImpl::encrypt(const std::string &in, std::string &out)
 {
 	unsigned int len = in.size();
-	out.resize(len+AES_BLOCK_SIZE);
+	out.resize(len+kIVSize);
 	encrypt((unsigned char*)in.data(), len, (unsigned char*)out.data());
 }
 
@@ -136,10 +152,13 @@ void Cipher::CipherImpl::decrypt(const unsigned char* in, const size_t &len, uns
 	
 	unsigned int num = 0;
 	
-	memcpy(dec_iv, in, AES_BLOCK_SIZE); // copy iv first
+	if(kIVSize != AES_BLOCK_SIZE)
+		memset(dec_iv, 0, AES_BLOCK_SIZE);
+	
+	memcpy(dec_iv+AES_BLOCK_SIZE-kIVSize, in, kIVSize); // copy iv first
 	
 	// now append the ciphertext
-    AES_ctr128_encrypt(in+AES_BLOCK_SIZE, out, len, &aes_enc_key_, dec_iv, ecount, &num);
+    AES_ctr128_encrypt(in+kIVSize, out, len, &aes_enc_key_, dec_iv, ecount, &num);
 	
 	// erase ecount to avoid (partial) recovery of the last block
 	memset(ecount, 0x00, AES_BLOCK_SIZE);
@@ -148,7 +167,7 @@ void Cipher::CipherImpl::decrypt(const unsigned char* in, const size_t &len, uns
 void Cipher::CipherImpl::decrypt(const std::string &in, std::string &out)
 {
 	unsigned int len = in.size();
-	out.resize(len-AES_BLOCK_SIZE);
+	out.resize(len-kIVSize);
 	decrypt((unsigned char*)in.data(), len, (unsigned char*)out.data());
 }
 	

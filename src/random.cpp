14a82d3035f2c19492c1d6c5170e05ac54102d9a
Raphael Bost <raphael_bost@alumni.brown.edu>
2015-11-20 15:02:04 +0100

    Forgot namespace

--- src/random.cpp
+++ src/random.cpp
@@ -2,9 +2,18 @@
 
 #include <fstream>
 
+namespace sse
+{
+
+namespace crypto
+{
+	
 void random_bytes(const size_t &byte_count, unsigned char* buffer)
 {
 	std::ifstream urandom("/dev/urandom", std::ios::in|std::ios::binary);
 	urandom.read((char*) buffer,byte_count);
 	urandom.close();
+}
+
+}
 }
--- src/random.hpp
+++ src/random.hpp
@@ -2,5 +2,13 @@
 
 #include <cstdint>
 
+namespace sse
+{
+
+namespace crypto
+{
+
 
 void random_bytes(const size_t &byte_count, unsigned char* buffer);
+}
+}

f23d7e6118d9769dd23dc7a2f944b13a0e37c202
Raphael Bost <raphael_bost@alumni.brown.edu>
2017-05-24 01:21:12 +0200

    Improved coverage for TDP (and fixed bugs)

--- tests/test_tdp.cpp
+++ tests/test_tdp.cpp
@@ -49,6 +49,13 @@ TEST(tdp, correctness)
         string dec = tdp_inv.invert(enc);
         
         ASSERT_EQ(sample, dec);
+        
+        
+        std::array<uint8_t, sse::crypto::Tdp::kMessageSize> enc_array;
+        ::copy(enc.begin(), enc.end(), enc_array.begin());
+        std::array<uint8_t, sse::crypto::Tdp::kMessageSize> dec_array = tdp_inv.invert(enc_array);
+
+        ASSERT_EQ(sample, std::string(dec_array.begin(), dec_array.end()));
     }
 }
 
@@ -146,4 +153,62 @@ TEST(tdp, multiple_inverse_2)
             ASSERT_EQ(v1, v2);
         }
     
+}
+
+TEST(tdp, copy)
+{
+    // check that deterministically generated values are consistent after copies
+    
+    sse::crypto::TdpInverse temp_inv_tdp;
+    sse::crypto::Tdp tdp_assign(temp_inv_tdp.public_key());
+    
+    for (size_t i = 0; i < TEST_COUNT; i++) {
+        sse::crypto::TdpInverse tdp_inv_orig;
+        sse::crypto::TdpInverse tdp_inv_copy(tdp_inv_orig);
+        sse::crypto::TdpInverse tdp_inv_sk_copy(tdp_inv_orig.private_key());
+        
+        sse::crypto::Tdp tdp_pk_copy(tdp_inv_orig.public_key());
+        sse::crypto::Tdp tdp_pk_copy_copy = tdp_pk_copy;
+
+        tdp_assign = tdp_pk_copy_copy;
+        
+        // check that they have the same public key
+        ASSERT_EQ(tdp_inv_copy.public_key(), tdp_inv_orig.public_key());
+        ASSERT_EQ(tdp_inv_sk_copy.public_key(), tdp_inv_orig.public_key());
+        ASSERT_EQ(tdp_pk_copy.public_key(), tdp_inv_orig.public_key());
+        ASSERT_EQ(tdp_pk_copy_copy.public_key(), tdp_inv_orig.public_key());
+        ASSERT_EQ(tdp_assign.public_key(), tdp_inv_orig.public_key());
+        
+        
+        std::string key = sse::crypto::random_string(16);
+        
+        for (size_t j = 0; j < 10; j++) {
+            string sample_orig = tdp_inv_orig.generate(key, std::to_string(j));
+            string sample_orig_copy = tdp_inv_orig.generate(key, std::to_string(j));
+            string sample_sk_copy = tdp_inv_orig.generate(key, std::to_string(j));
+            string sample_pk_copy = tdp_pk_copy.generate(key, std::to_string(j));
+            string sample_eq = tdp_pk_copy_copy.generate(key, std::to_string(j));
+            string sample_assign = tdp_assign.generate(key, std::to_string(j));
+
+            ASSERT_EQ(sample_orig_copy, sample_orig);
+            ASSERT_EQ(sample_sk_copy, sample_orig);
+            ASSERT_EQ(sample_pk_copy, sample_orig);
+            ASSERT_EQ(sample_eq, sample_orig);
+            ASSERT_EQ(sample_assign, sample_orig);
+        }
+    }
+}
+
+TEST(tdp, exceptions)
+{
+    
+    ASSERT_THROW(sse::crypto::Tdp tdp(" "), std::runtime_error);
+    ASSERT_THROW(sse::crypto::TdpInverse tdp_inv(" "), std::runtime_error);
+    
+    sse::crypto::TdpInverse tdp_inv;
+    
+    std::string out;
+    
+    ASSERT_THROW(tdp_inv.invert(" ", out), std::invalid_argument);
+
 }

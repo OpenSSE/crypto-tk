1a8717fbab187e4d0898c97cb0fc4bd36f49a156
Raphael Bost <raphael_bost@alumni.brown.edu>
2017-11-27 19:38:42 +0100

    Use libsodium to generate randomness

--- src/random.cpp
+++ src/random.cpp
@@ -20,15 +20,7 @@
 
 #include "random.hpp"
 
-#include "aesni/aesni.hpp"
-
-#include <cstring>
-#include <algorithm>
-#include <array>
-#include <fstream>
-
-#include <sodium/utils.h>
-#include <openssl/aes.h>
+#include <sodium/randombytes.h>
 
 namespace sse
 {
@@ -36,144 +28,10 @@ namespace sse
 namespace crypto
 {
 	
-class Drbg::DrbgImpl
-{
-public:
-#if USE_AESNI
-    typedef aes_subkeys_type key_type;
-#else
-    typedef AES_KEY key_type;
-#endif
-    
-
-	DrbgImpl();
-	~DrbgImpl();
-	
-	void fill();
-	void reseed();
-	void next(const size_t &byte_count, unsigned char* out);
-
-private:
-    static constexpr uint64_t kBlockCount = 64;
-    
-	key_type aes_key_;
-    std::array<unsigned char,16*kBlockCount> buffer_;
-    
-#if USE_AESNI
-    uint64_t iv_;
-#else
-    unsigned char iv_[AES_BLOCK_SIZE];
-#endif
-    
-    size_t buffer_pos_;
-    size_t remaining_bytes_;
-	
-	static size_t kReseedBytesCount;
-};
-
-size_t Drbg::DrbgImpl::kReseedBytesCount = 1024 * 1024 * 1024; // 1 GiB
-
-
-Drbg::Drbg() : drdg_imp_(new DrbgImpl())
-{
-}
-
-Drbg::~Drbg()
-{
-	delete drdg_imp_;
-}
-
-void Drbg::reseed()
-{
-	drdg_imp_->reseed();
-}	
-	
-void Drbg::next(const size_t &byte_count, unsigned char* out)
-{
-	drdg_imp_->next(byte_count, out);
-}
-
-
-Drbg::DrbgImpl::DrbgImpl()
-    : aes_key_{}, buffer_{}, iv_{}, buffer_pos_(0), remaining_bytes_(0)
-{
-	reseed();
-	fill();
-}
-
-Drbg::DrbgImpl::~DrbgImpl()
-{
-	// erase subkeys
-    sodium_memzero(&aes_key_, sizeof(AES_KEY));
-}
-
-void Drbg::DrbgImpl::reseed()
-{
-	unsigned char* key_buf = new unsigned char[16];
-	std::ifstream urandom("/dev/urandom", std::ios::in|std::ios::binary);
-	urandom.read((char*) key_buf,16);
-	urandom.close();
-
-#if USE_AESNI
-    aes_key_ = aesni_derive_subkeys(key_buf);
-    iv_ = 0;
-#else
-	if (AES_set_encrypt_key(key_buf, 128, &aes_key_) != 0)
-	{
-		// throw an exception
-		throw std::runtime_error("Unable to init AES subkeys");
-	}
-    sodium_memzero(iv_, sizeof(AES_BLOCK_SIZE));
-
-#endif
-
-
-	// erase the key buffer
-    sodium_memzero(key_buf, 16);
-
-	delete [] key_buf;
-	
-	remaining_bytes_ = kReseedBytesCount;
-}
-	
-void Drbg::DrbgImpl::fill()
-{
-	if(remaining_bytes_ < buffer_.size())
-	{
-		reseed();
-	}
-	
-	remaining_bytes_ -= buffer_.size();
-	
-	
-#if USE_AESNI
-    aesni_ctr(kBlockCount, iv_, aes_key_.data(), buffer_.data());
-    iv_ += kBlockCount;
-#else
-    unsigned char ecount[AES_BLOCK_SIZE];
-    // memset(ecount, 0x00, AES_BLOCK_SIZE);
-    
-    unsigned int num = 0;
-
-    AES_ctr128_encrypt(buffer_.data(), buffer_.data(), buffer_.size(), &aes_key_, iv_, ecount, &num);
-#endif
-    buffer_pos_ = 0;
-}
-	
-void Drbg::DrbgImpl::next(const size_t &byte_count, unsigned char* out)
-{
-	size_t pos = 0;
-    while (pos < byte_count) {
-      size_t n = std::min(buffer_.size() - buffer_pos_, byte_count);
-      memcpy(out, buffer_.data() + buffer_pos_, n);
-      buffer_pos_ += n;
-	  pos += n;
-	  
-      if (buffer_pos_ == buffer_.size())
-        fill();
+    void random_bytes(const size_t byte_count, unsigned char* out)
+    {
+        randombytes_buf(out, byte_count);
     }
-	
-}
-
+    
 }
 }

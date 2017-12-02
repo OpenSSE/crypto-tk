92711e30549147d2f7189c30c979a43b471f3f50
Raphael Bost <raphael_bost@alumni.brown.edu>
2017-12-02 17:00:52 +0100

    Add license headers to some forgotten files

--- src/key.hpp
+++ src/key.hpp
@@ -1,9 +1,21 @@
 //
-//  key.hpp
-//  libsse_crypto
+// libsse_crypto - An abstraction layer for high level cryptographic features.
+// Copyright (C) 2015-2017 Raphael Bost
 //
-//  Created by Raphael Bost on 23/10/2017.
-//  Copyright © 2017 VSSE project. All rights reserved.
+// This file is part of libsse_crypto.
+//
+// libsse_crypto is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// libsse_crypto is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with libsse_crypto.  If not, see <http://www.gnu.org/licenses/>.
 //
 
 #pragma once

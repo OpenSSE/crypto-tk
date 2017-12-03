78dec0a1f6f58a84439f77fb97543d3ef322de81
Raphael Bost <raphael_bost@alumni.brown.edu>
2017-12-03 17:19:24 +0100

    Add a script to easily run clang-tidy and the associated .clang-tidy file

---
+++ scripts/tidy.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+if [[ -z $CLANG_TIDY ]]; then
+	CLANG_TIDY="clang-tidy"
+fi
+
+COMPILE_OPTS="-std=c++14 -Weffc++ -Woverloaded-virtual -Wsign-promo -Wstrict-overflow=5 -D CHECK_TEMPLATE_INSTANTIATION -Wno-effc++ -march=native -fPIC -Wall -Wcast-qual -Wdisabled-optimization -Wformat=2 -Wmissing-declarations -Wmissing-include-dirs -Wredundant-decls -Wshadow -Wstrict-overflow=5 -Wdeprecated -Wno-unused-function -O2 -DWITH_OPENSSL -Ibuild -Isrc -I/usr/local/opt/openssl/include"
+
+LINE_FILTER="''"
+
+eval "$CLANG_TIDY src/*.{hpp,cpp} src/*/*.{hpp,cpp} src/*/*/*.{hpp,cpp} -line-filter=$LINE_FILTER -- $COMPILE_OPTS"

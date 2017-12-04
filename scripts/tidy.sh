a953a6e85986dab2fccaba0d2da785d103e288f3
Raphael Bost <raphael_bost@alumni.brown.edu>
2017-12-04 23:24:11 +0100

    Improve clang-tidy script

--- scripts/tidy.sh
+++ scripts/tidy.sh
@@ -3,8 +3,10 @@ if [[ -z $CLANG_TIDY ]]; then
 	CLANG_TIDY="clang-tidy"
 fi
 
+GLOBIGNORE='**/boost/**:boost/**:**/boost' # do not look into boost headers
+
 COMPILE_OPTS="-std=c++14 -Weffc++ -Woverloaded-virtual -Wsign-promo -Wstrict-overflow=5 -D CHECK_TEMPLATE_INSTANTIATION -Wno-effc++ -march=native -fPIC -Wall -Wcast-qual -Wdisabled-optimization -Wformat=2 -Wmissing-declarations -Wmissing-include-dirs -Wredundant-decls -Wshadow -Wstrict-overflow=5 -Wdeprecated -Wno-unused-function -O2 -DWITH_OPENSSL -Ibuild -Isrc -I/usr/local/opt/openssl/include"
 
 LINE_FILTER="''"
 
-eval "$CLANG_TIDY src/*.{hpp,cpp} src/*/*.{hpp,cpp} src/*/*/*.{hpp,cpp} -line-filter=$LINE_FILTER -- $COMPILE_OPTS"
+eval "$CLANG_TIDY src/**/*.{hpp,cpp} -line-filter=$LINE_FILTER -- $COMPILE_OPTS"

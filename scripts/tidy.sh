d8f46b57946309ad57cfac5d9f03e49ba0ead1f0
Raphael Bost <raphael_bost@alumni.brown.edu>
2017-12-08 15:12:41 +0100

    Run clang-tidy on C files too

--- scripts/tidy.sh
+++ scripts/tidy.sh
@@ -4,9 +4,23 @@ if [[ -z $CLANG_TIDY ]]; then
 fi
 
 GLOBIGNORE='**/boost/**:boost/**:**/boost' # do not look into boost headers
+GLOBIGNORE=$GLOBIGNORE:'**/mbedtls/**' # do not look into boost headers
 
-COMPILE_OPTS="-std=c++14 -Weffc++ -Woverloaded-virtual -Wsign-promo -Wstrict-overflow=5 -D CHECK_TEMPLATE_INSTANTIATION -Wno-effc++ -march=native -fPIC -Wall -Wcast-qual -Wdisabled-optimization -Wformat=2 -Wmissing-declarations -Wmissing-include-dirs -Wredundant-decls -Wshadow -Wstrict-overflow=5 -Wdeprecated -Wno-unused-function -O2 -DWITH_OPENSSL -Ibuild -Isrc -I/usr/local/opt/openssl/include"
+echo $GLOBIGNORE
+
+INCLUDES="-Ibuild -Isrc -I/usr/local/opt/openssl/include"
+
+DEFINES="-DCHECK_TEMPLATE_INSTANTIATION  -DWITH_OPENSSL"
+
+CFLAGS="-march=native -O2 -fPIC -Wall -Wcast-qual -Wdisabled-optimization -Wformat=2 -Wmissing-declarations -Wmissing-include-dirs -Wredundant-decls -Wshadow -Wstrict-overflow=5 -Wdeprecated -Wno-unused-function"
+
+CCFLAGS="-std=c99"
+
+CXXFLAGS="-std=c++14 -Weffc++ -Wnon-virtual-dtor -Woverloaded-virtual -Wsign-promo"
 
 LINE_FILTER="''"
 
-eval "$CLANG_TIDY src/**/*.{hpp,cpp} -line-filter=$LINE_FILTER -- $COMPILE_OPTS"
+eval "$CLANG_TIDY src/**/*.{h,c} -line-filter=$LINE_FILTER -- $CFLAGS $CCFLAGS $INCLUDES"
+
+eval "$CLANG_TIDY src/**/*.{hpp,cpp} -line-filter=$LINE_FILTER -- $CFLAGS $CXXFLAGS $INCLUDES"
+

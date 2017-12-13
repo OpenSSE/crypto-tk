f834c7165cc9a6d5f6b0e0c25594179824342ba3
Raphael Bost <raphael_bost@alumni.brown.edu>
2017-12-13 23:35:17 +0100

    Document and re-format the Cipher class

--- src/cipher.cpp
+++ src/cipher.cpp
@@ -24,206 +24,228 @@
 
 #include <exception>
 
-#include <sodium/utils.h>
-#include <sodium/crypto_generichash_blake2b.h>
 #include <sodium/crypto_aead_chacha20poly1305.h>
+#include <sodium/crypto_generichash_blake2b.h>
+#include <sodium/utils.h>
 
-namespace sse
-{
+namespace sse {
 
-namespace crypto
-{
-    
+namespace crypto {
+
 #define NONCE_SIZE crypto_generichash_blake2b_SALTBYTES
 
-static constexpr uint8_t hash_personal__[crypto_generichash_blake2b_PERSONALBYTES] = "encryption_key";
-    
+static constexpr uint8_t
+    hash_personal__[crypto_generichash_blake2b_PERSONALBYTES]
+    = "encryption_key";
+
 class Cipher::CipherImpl
 {
 public:
-
     CipherImpl() = delete;
-	
-	explicit CipherImpl(Key<kKeySize>&& k);
-	
-	~CipherImpl() = default;
 
-    
-    inline static size_t ciphertext_length(const size_t plaintext_len)
+    explicit CipherImpl(Key<kKeySize>&& k) noexcept;
+
+    ~CipherImpl() = default;
+
+    inline static size_t ciphertext_length(const size_t plaintext_len) noexcept
     {
-        return plaintext_len+NONCE_SIZE+crypto_aead_chacha20poly1305_IETF_ABYTES;
+        return plaintext_len + NONCE_SIZE
+               + crypto_aead_chacha20poly1305_IETF_ABYTES;
     };
-    
-    inline static size_t plaintext_length(const size_t c_len)
+
+    inline static size_t plaintext_length(const size_t c_len) noexcept
     {
-        if (c_len > NONCE_SIZE+crypto_aead_chacha20poly1305_IETF_ABYTES) {
-            return c_len-NONCE_SIZE-crypto_aead_chacha20poly1305_IETF_ABYTES;
+        if (c_len > NONCE_SIZE + crypto_aead_chacha20poly1305_IETF_ABYTES) {
+            return c_len - NONCE_SIZE
+                   - crypto_aead_chacha20poly1305_IETF_ABYTES;
         }
         return 0;
     };
-    
-	void encrypt(const unsigned char* in, const size_t &len, unsigned char* out);
-	void encrypt(const std::string &in, std::string &out);
-	void decrypt(const unsigned char* in, const size_t &len, unsigned char* out);
-	void decrypt(const std::string &in, std::string &out);
 
+    void
+         encrypt(const unsigned char* in, const size_t& len, unsigned char* out);
+    void encrypt(const std::string& in, std::string& out);
+    void
+         decrypt(const unsigned char* in, const size_t& len, unsigned char* out);
+    void decrypt(const std::string& in, std::string& out);
 
 private:
-    static_assert(crypto_generichash_blake2b_KEYBYTES == kKeySize, "Invalid Cipher key size");
-	Key<crypto_generichash_blake2b_KEYBYTES> key_;
+    static_assert(crypto_generichash_blake2b_KEYBYTES == kKeySize,
+                  "Invalid Cipher key size");
+    Key<crypto_generichash_blake2b_KEYBYTES> key_;
 };
-	
 
-    Cipher::Cipher(Key<kKeySize>&& k) : cipher_imp_(new CipherImpl(std::move(k)))
-{	
-}
-
-Cipher::~Cipher() 
-{ 
-	delete cipher_imp_;
-}
-
-void Cipher::encrypt(const std::string &in, std::string &out)
+Cipher::Cipher(Key<kKeySize>&& k) : cipher_imp_(new CipherImpl(std::move(k)))
 {
-	cipher_imp_->encrypt(in, out);
-}
-void Cipher::decrypt(const std::string &in, std::string &out)
-{
-	cipher_imp_->decrypt(in, out);
 }
 
-size_t Cipher::ciphertext_length(const size_t plaintext_len)
+Cipher::~Cipher()
+{
+    delete cipher_imp_;
+}
+
+void Cipher::encrypt(const std::string& in, std::string& out)
+{
+    cipher_imp_->encrypt(in, out);
+}
+void Cipher::decrypt(const std::string& in, std::string& out)
+{
+    cipher_imp_->decrypt(in, out);
+}
+
+size_t Cipher::ciphertext_length(const size_t plaintext_len) noexcept
 {
     return Cipher::CipherImpl::ciphertext_length(plaintext_len);
 }
-    
-size_t Cipher::plaintext_length(const size_t c_len)
+
+size_t Cipher::plaintext_length(const size_t c_len) noexcept
 {
     return Cipher::CipherImpl::plaintext_length(c_len);
 }
 
 // Cipher implementation
 
-#define MIN(a,b) (((a) > (b)) ? (b) : (a))
+#define MIN(a, b) (((a) > (b)) ? (b) : (a))
 
 // Compute maximum number of blocks that can be encrypted with the same key
-// This number comes from the security reduction of CTR mode (2^48 blocks at most to retain 32 bits of security)
-// and from the IV length (not more than 2^(8*kIVSize) different IVs)
+// This number comes from the security reduction of CTR mode (2^48 blocks at
+// most to retain 32 bits of security) and from the IV length (not more than
+// 2^(8*kIVSize) different IVs)
 
-Cipher::CipherImpl::CipherImpl(Key<kKeySize>&& k)
-    : key_(std::move(k))
+Cipher::CipherImpl::CipherImpl(Key<kKeySize>&& k) noexcept : key_(std::move(k))
 {
 }
 
-void Cipher::CipherImpl::encrypt(const unsigned char* in, const size_t &len, unsigned char* out)
+void Cipher::CipherImpl::encrypt(const unsigned char* in,
+                                 const size_t&        len,
+                                 unsigned char*       out)
 {
     if (len == 0) {
-        throw std::invalid_argument("The minimum number of bytes to encrypt is 1.");
+        throw std::invalid_argument(
+            "The minimum number of bytes to encrypt is 1.");
     }
-    
-    uint8_t chacha_key[crypto_aead_chacha20poly1305_KEYBYTES];
+
+    uint8_t            chacha_key[crypto_aead_chacha20poly1305_KEYBYTES];
     unsigned long long c_len = 0;
-    
+
     // generate a random nonce, and place it at the beginning of the output
     random_bytes(NONCE_SIZE, out);
-    
+
     // unlock the master key
     key_.unlock();
-    
-    // start by deriving a subkey from the master and the nonce
-    crypto_generichash_blake2b_salt_personal(chacha_key, sizeof(chacha_key),
-                                                 nullptr, 0,
-                                                 key_.data(), kKeySize,
-                                                 out, hash_personal__);
 
-    
+    // start by deriving a subkey from the master and the nonce
+    crypto_generichash_blake2b_salt_personal(chacha_key,
+                                             sizeof(chacha_key),
+                                             nullptr,
+                                             0,
+                                             key_.data(),
+                                             kKeySize,
+                                             out,
+                                             hash_personal__);
+
     // re-lock the master key
     key_.lock();
-    
+
     // go for encryption with the derived key
-    crypto_aead_chacha20poly1305_ietf_encrypt(out+NONCE_SIZE, &c_len,
-                                              in, len,
-                                              nullptr, 0,
-                                              nullptr, out, chacha_key);
-    
-    
-    
-    
+    crypto_aead_chacha20poly1305_ietf_encrypt(out + NONCE_SIZE,
+                                              &c_len,
+                                              in,
+                                              len,
+                                              nullptr,
+                                              0,
+                                              nullptr,
+                                              out,
+                                              chacha_key);
+
     // delete the derived key
     sodium_memzero(chacha_key, crypto_aead_chacha20poly1305_KEYBYTES);
 }
 
-void Cipher::CipherImpl::encrypt(const std::string &in, std::string &out)
+void Cipher::CipherImpl::encrypt(const std::string& in, std::string& out)
 {
-    size_t len = in.size();
-	size_t c_len = ciphertext_length(len);
-    unsigned char *data = new unsigned char[c_len];
+    size_t         len   = in.size();
+    size_t         c_len = ciphertext_length(len);
+    unsigned char* data  = new unsigned char[c_len];
 
-	encrypt((const unsigned char*)in.data(), len, data);
-    out = std::string((char *)data, c_len);
+    encrypt((const unsigned char*)in.data(), len, data);
+    out = std::string((char*)data, c_len);
 
     // erase the buffer
     sodium_memzero(data, c_len);
-    
-    delete [] data;
+
+    delete[] data;
 }
 
-void Cipher::CipherImpl::decrypt(const unsigned char* in, const size_t &len, unsigned char* out)
+void Cipher::CipherImpl::decrypt(const unsigned char* in,
+                                 const size_t&        len,
+                                 unsigned char*       out)
 {
     if (len < ciphertext_length(0)) {
-        throw std::invalid_argument("The minimum number of bytes to decrypt is " + std::to_string(ciphertext_length(0))); /* LCOV_EXCL_LINE */
+        throw std::invalid_argument(
+            "The minimum number of bytes to decrypt is "
+            + std::to_string(ciphertext_length(0))); /* LCOV_EXCL_LINE */
     }
 
-    uint8_t chacha_key[crypto_aead_chacha20poly1305_KEYBYTES];
+    uint8_t            chacha_key[crypto_aead_chacha20poly1305_KEYBYTES];
     unsigned long long m_len = 0;
-    
+
     // unlock the master key
     key_.unlock();
-    
+
     // start by deriving a subkey from the master and the nonce
-    crypto_generichash_blake2b_salt_personal(chacha_key, sizeof(chacha_key),
-                                             nullptr, 0,
-                                             key_.data(), kKeySize,
-                                             in, hash_personal__);
-    
-    
+    crypto_generichash_blake2b_salt_personal(chacha_key,
+                                             sizeof(chacha_key),
+                                             nullptr,
+                                             0,
+                                             key_.data(),
+                                             kKeySize,
+                                             in,
+                                             hash_personal__);
+
     // re-lock the master key
     key_.lock();
 
     // go for decryption with the derived key
-    int ret = crypto_aead_chacha20poly1305_ietf_decrypt(out, &m_len,
-                                                        nullptr, in+NONCE_SIZE, len-NONCE_SIZE,
-                                                        nullptr, 0,
-                                                        in, chacha_key);
-    
+    int ret = crypto_aead_chacha20poly1305_ietf_decrypt(out,
+                                                        &m_len,
+                                                        nullptr,
+                                                        in + NONCE_SIZE,
+                                                        len - NONCE_SIZE,
+                                                        nullptr,
+                                                        0,
+                                                        in,
+                                                        chacha_key);
+
     // delete the derived key
     sodium_memzero(chacha_key, crypto_aead_chacha20poly1305_KEYBYTES);
 
     if (ret == -1) { // invalid decryption
         // erase the decrypted plaintext
         sodium_memzero(out, m_len);
-        
+
         throw std::runtime_error("Failed decryption. Invalid ciphertext");
     }
 }
 
-void Cipher::CipherImpl::decrypt(const std::string &in, std::string &out)
+void Cipher::CipherImpl::decrypt(const std::string& in, std::string& out)
 {
-	size_t len = in.size();
-    
-    if (len <= NONCE_SIZE+crypto_aead_chacha20poly1305_IETF_ABYTES) {
-        throw std::invalid_argument("The minimum number of bytes to decrypt is 1. The minimum length for a decryption input is kIVSize+1");
+    size_t len = in.size();
+
+    if (len <= NONCE_SIZE + crypto_aead_chacha20poly1305_IETF_ABYTES) {
+        throw std::invalid_argument("The minimum number of bytes to decrypt is "
+                                    "1. The minimum length for a "
+                                    "decryption input is kIVSize+1");
     }
-    
+
     size_t p_len = plaintext_length(len);
 
-    unsigned char *data = new unsigned char[p_len];
-	decrypt((const unsigned char*)in.data(), len, data);
+    unsigned char* data = new unsigned char[p_len];
+    decrypt((const unsigned char*)in.data(), len, data);
 
-    out = std::string((char *)data, p_len);
-    delete [] data;
+    out = std::string((char*)data, p_len);
+    delete[] data;
 }
-	
 
-}
-}
+} // namespace crypto
+} // namespace sse
--- src/cipher.hpp
+++ src/cipher.hpp
@@ -27,53 +27,121 @@
 #include <array>
 #include <string>
 
-namespace sse
-{
+namespace sse {
 
-namespace crypto
-{
+namespace crypto {
 
-/*****
-* Cipher class
-*
-* Opaque class for encryption and decryption.
-* 	
-* For now, Cipher implements a counter mode.
-******/
+/// @class Cipher
+/// @brief Encryption and decryption.
+///
+/// Cipher is an opaque class for symmetric encryption and decryption.
+/// It implements a Chacha20+Poly1305 with a nonce-derived key.
+/// This allows for larger nonces (128 bits) than the original
+/// Chacha20+Poly1305 construction (96 bits). As a consequence,
+/// nonces can be randomly generated, and the Cipher object does not
+/// need to keep a state to be secure.
+///
 
 class Cipher
 {
 public:
-	static constexpr uint8_t kKeySize = 32;
+    /// @brief Cipher key size (in bytes)
+    static constexpr uint8_t kKeySize = 32;
 
-	Cipher() = delete;
-	
-	// we should not be able to duplicate Cipher objects
-	Cipher(const Cipher& c) = delete;
-	Cipher(Cipher& c) = delete;
-	Cipher(const Cipher&& c) = delete;
-	Cipher(Cipher&& c) = delete;
-	
-	
-	explicit Cipher(Key<kKeySize>&& k);
-	
-	~Cipher();
+    Cipher() = delete;
 
-	void encrypt(const std::string &in, std::string &out);
-	void decrypt(const std::string &in, std::string &out);
-	
-	// Again, avoid any assignement of Cipher objects
-	Cipher& operator=(const Cipher& h) = delete;
-	Cipher& operator=(Cipher& h) = delete;
-	
-    
-    static size_t ciphertext_length(const size_t plaintext_len);
-    static size_t plaintext_length(const size_t c_len);
+    // we should not be able to duplicate Cipher objects
+    Cipher(const Cipher& c)  = delete;
+    Cipher(Cipher& c)        = delete;
+    Cipher(const Cipher&& c) = delete;
+    Cipher(Cipher&& c)       = delete;
 
-private:	
-	class CipherImpl; // not defined in the header
-	CipherImpl *cipher_imp_; // opaque pointer
+    // Again, avoid any assignement of Cipher objects
+    Cipher& operator=(const Cipher& h) = delete;
+    Cipher& operator=(Cipher& h) = delete;
+
+    ///
+    /// @brief Constructor
+    ///
+    /// Creates a cipher from a 32 bytes (256 bits) key.
+    /// After a call to the constructor, the input key is
+    /// held by the Cipher object, and cannot be re-used.
+    ///
+    /// @param k    The key used to initialize the cipher.
+    ///             Upon return, k is empty
+    ///
+    explicit Cipher(Key<kKeySize>&& k);
+
+    ///
+    /// @brief Destructor
+    ///
+    /// Destructs the Cipher object and erase its key.
+    ///
+    ///
+    ~Cipher();
+
+    ///
+    /// @brief Encrypt a plaintext
+    ///
+    /// Computes the encryption of the input plaintext.
+    ///
+    /// @param in    The plaintext to be encrypted. in must be of size 1 at
+    /// least (i.e. non-empty).
+    /// @param out   The computed ciphertext.
+    ///
+    /// @exception std::invalid_argument The size of in is 0.
+    ///
+    void encrypt(const std::string& in, std::string& out);
+
+    ///
+    /// @brief Decrypt a ciphertext
+    ///
+    /// Computes the plaintext corresponding to the input ciphertext.
+    ///
+    /// @param in    The ciphertext to be decrypted. in must be of size 1 at
+    /// least (i.e. non-empty).
+    /// @param out   The computed plaintext.
+    ///
+    /// @exception std::invalid_argument in is smaller than the size of the
+    /// nonce
+    /// + the size of the tag.
+    /// @exception std::runtime_error       The decryption failed: invalid tag
+    ///
+    void decrypt(const std::string& in, std::string& out);
+
+    ///
+    /// @brief Compute the length of a ciphertext
+    ///
+    /// Computes the size of the ciphertext returned by encrypt
+    /// given the plaintext length.
+    ///
+    /// @param plaintext_len    Length of the plaintext to be encrypted.
+    ///
+    /// @return Length of the ciphertext, that is the length of the plaintext +
+    /// the length of the nonce + the length of the tag
+    ///
+    static size_t ciphertext_length(const size_t plaintext_len) noexcept;
+
+    ///
+    /// @brief Compute the length of a plaintext
+    ///
+    /// Computes the size of the plaintext returned by decrypt
+    /// given the ciphertext length when the decryption suceeds.
+    ///
+    /// @param c_len    Length of the ciphertext to be decrypted.
+    ///
+    /// @return Length of the plaintext, that is the length of the ciphertext -
+    /// the length of the nonce - the length of the tag (or 0 if this quantity
+    /// is negative)
+    ///
+    static size_t plaintext_length(const size_t c_len) noexcept;
+
+private:
+	/// @class CipherImpl
+    /// @brief Hidden Cipher implementation
+    class CipherImpl;
+    CipherImpl* cipher_imp_; // opaque pointer
 };
 
-}
-}
+} // namespace crypto
+} // namespace sse

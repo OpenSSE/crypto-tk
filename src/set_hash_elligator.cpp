a95680c03906c06ab2969ccb8f436351730b6d31
Raphael Bost <raphael_bost@alumni.brown.edu>
2017-12-20 10:09:12 +0100

    Add a basic implementation of Elligator-based multiset hashing

---
+++ src/set_hash_elligator.cpp
@@ -0,0 +1,295 @@
+//
+// libsse_crypto - An abstraction layer for high level cryptographic features.
+// Copyright (C) 2015-2017 Raphael Bost
+//
+// This file is part of libsse_crypto.
+//
+// libsse_crypto is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// libsse_crypto is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with libsse_crypto.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#include "set_hash_elligator.hpp"
+
+#include <exception>
+#include <iomanip>
+#include <iostream>
+
+#include <sodium/crypto_core_ed25519.h>
+#include <sodium/crypto_scalarmult_ed25519.h>
+#include <sodium/utils.h>
+
+#include "ecmh/array_view/array_view.hpp"
+#include "ecmh/binary_elliptic_curve/GLS254.hpp"
+#include "ecmh/multiset_hash/ECMH.hpp"
+#include "hash.hpp"
+
+using namespace jbms::multiset_hash;
+
+namespace sse {
+
+namespace crypto {
+
+/*
+ * HashWrapper
+ *
+ * A wrapper around the Hash class to make it compatible with the ECMH code.
+ *
+ */
+
+
+constexpr uint8_t scalar_zero__[crypto_scalarmult_ed25519_SCALARBYTES]
+    = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+       0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+       0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+
+constexpr uint8_t ec_inf_point__[crypto_core_ed25519_BYTES]
+    = {0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+       0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+       0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+
+class SetHash_Elligator::SetHashImpl
+{
+    friend SetHash_Elligator;
+
+public:
+    SetHashImpl();
+    explicit SetHashImpl(const SetHashImpl& s);
+    explicit SetHashImpl(const std::array<uint8_t, kSetHashSize>& data);
+    explicit SetHashImpl(const std::vector<std::string>& in_set);
+
+    void add_element(const std::string& in);
+    void add_set(const SetHashImpl* in);
+    void remove_element(const std::string& in);
+    void remove_set(const SetHashImpl* in);
+
+    std::array<uint8_t, kSetHashSize> data() const;
+    bool                              operator==(const SetHashImpl& h) const;
+
+private:
+    static void gen_curve_point(
+        std::array<uint8_t, crypto_core_ed25519_BYTES>& p,
+        const uint8_t*                                  buf,
+        const size_t                                    len);
+
+    uint8_t
+        ellig_state_[crypto_core_ed25519_BYTES]; // keep it that way for now,
+                                                 // might be replaced with
+                                                 // dynamic memory allocation
+    static constexpr std::array<uint8_t, crypto_scalarmult_ed25519_SCALARBYTES>
+        seed__{{0x00}};
+};
+
+constexpr std::array<uint8_t, crypto_scalarmult_ed25519_SCALARBYTES>
+    SetHash_Elligator::SetHashImpl::seed__;
+
+SetHash_Elligator::SetHash_Elligator() : set_hash_imp_(new SetHashImpl())
+{
+}
+
+SetHash_Elligator::SetHash_Elligator(
+    const std::array<uint8_t, kSetHashSize>& data)
+    : set_hash_imp_(new SetHashImpl(data))
+{
+}
+
+SetHash_Elligator::SetHash_Elligator(const SetHash_Elligator& o)
+    : set_hash_imp_(new SetHashImpl(*o.set_hash_imp_))
+{
+}
+
+SetHash_Elligator::SetHash_Elligator(SetHash_Elligator&& o) noexcept
+    : set_hash_imp_(o.set_hash_imp_)
+{
+    o.set_hash_imp_ = NULL;
+}
+
+SetHash_Elligator::SetHash_Elligator(const std::vector<std::string>& in_set)
+    : set_hash_imp_(new SetHashImpl(in_set))
+{
+}
+
+SetHash_Elligator::~SetHash_Elligator()
+{
+    if (set_hash_imp_ != nullptr) {
+        delete set_hash_imp_;
+    }
+}
+
+void SetHash_Elligator::add_element(const std::string& in)
+{
+    set_hash_imp_->add_element(in);
+}
+
+void SetHash_Elligator::add_set(const SetHash_Elligator& h)
+{
+    set_hash_imp_->add_set(h.set_hash_imp_);
+}
+
+void SetHash_Elligator::remove_element(const std::string& in)
+{
+    set_hash_imp_->remove_element(in);
+}
+
+void SetHash_Elligator::remove_set(const SetHash_Elligator& h)
+{
+    set_hash_imp_->remove_set(h.set_hash_imp_);
+}
+
+std::array<uint8_t, SetHash_Elligator::kSetHashSize> SetHash_Elligator::data()
+    const
+{
+    return set_hash_imp_->data();
+}
+
+
+/* LCOV_EXCL_START */
+std::ostream& operator<<(std::ostream& os, const SetHash_Elligator& h)
+{
+    auto d = h.set_hash_imp_->data();
+    for (uint8_t b : d) {
+        os << std::hex << std::setw(2) << std::setfill('0') << (uint)b;
+    }
+    os << std::dec;
+    return os;
+}
+/* LCOV_EXCL_STOP */
+
+SetHash_Elligator& SetHash_Elligator::operator=(const SetHash_Elligator& h)
+{
+    if ((this != &h) && (set_hash_imp_ != h.set_hash_imp_)) {
+        delete set_hash_imp_;
+        set_hash_imp_ = new SetHashImpl(*h.set_hash_imp_);
+    }
+    return *this;
+}
+
+bool SetHash_Elligator::operator==(const SetHash_Elligator& h) const
+{
+    return (*set_hash_imp_ == *h.set_hash_imp_);
+}
+
+bool SetHash_Elligator::operator!=(const SetHash_Elligator& h) const
+{
+    return !(*this == h);
+}
+
+/*
+ * SetHashImpl
+ *
+ */
+
+void SetHash_Elligator::SetHashImpl::gen_curve_point(
+    std::array<uint8_t, crypto_core_ed25519_BYTES>& p,
+    const uint8_t*                                  buf,
+    const size_t                                    len)
+{
+    std::array<uint8_t, crypto_core_ed25519_UNIFORMBYTES> h;
+    sse::crypto::Hash::hash(
+        buf, len, crypto_core_ed25519_UNIFORMBYTES, h.data());
+
+    crypto_core_ed25519_from_uniform(p.data(), h.data());
+}
+
+SetHash_Elligator::SetHashImpl::SetHashImpl()
+{
+    // the initial state is the infinite point
+    // we directly copy ec_inf_point__: calling
+    // crypto_scalarmult_ed25519_base(ellig_state_, scalar_zero__)
+    // will generate a differenter byte string as the one obtained by computing
+    // crypto_core_ed25519_sub(_,b,b) (computing b-b). Indeed,
+    // crypto_core_ed25519_sub(x,b,b) sets x to ec_inf_point__.
+    memcpy(ellig_state_, ec_inf_point__, crypto_core_ed25519_BYTES);
+}
+
+SetHash_Elligator::SetHashImpl::SetHashImpl(
+    const SetHash_Elligator::SetHashImpl& s)
+{
+    memcpy(ellig_state_, s.ellig_state_, sizeof(ellig_state_));
+}
+
+SetHash_Elligator::SetHashImpl::SetHashImpl(
+    const std::array<uint8_t, kSetHashSize>& data)
+{
+    memcpy(ellig_state_, data.data(), crypto_core_ed25519_BYTES);
+
+    if ((crypto_core_ed25519_is_valid_point(ellig_state_) != 1)
+        || (sodium_memcmp(
+                data.data(), ec_inf_point__, crypto_core_ed25519_BYTES)
+            != 0)) {
+        throw std::invalid_argument("SetHash: Invalid curve point");
+    }
+}
+
+SetHash_Elligator::SetHashImpl::SetHashImpl(
+    const std::vector<std::string>& in_set)
+{
+    memcpy(ellig_state_, ec_inf_point__, crypto_core_ed25519_BYTES);
+    std::array<uint8_t, crypto_core_ed25519_BYTES> p;
+
+    for (auto& s : in_set) {
+        SetHash_Elligator::SetHashImpl::gen_curve_point(
+            p, reinterpret_cast<const uint8_t*>(s.data()), s.size());
+
+        crypto_core_ed25519_add(ellig_state_, ellig_state_, p.data());
+    }
+}
+
+void SetHash_Elligator::SetHashImpl::add_element(const std::string& in)
+{
+    std::array<uint8_t, crypto_core_ed25519_BYTES> p;
+    SetHash_Elligator::SetHashImpl::gen_curve_point(
+        p, reinterpret_cast<const uint8_t*>(in.data()), in.size());
+
+    crypto_core_ed25519_add(ellig_state_, ellig_state_, p.data());
+}
+
+void SetHash_Elligator::SetHashImpl::add_set(
+    const SetHash_Elligator::SetHashImpl* in)
+{
+    crypto_core_ed25519_add(ellig_state_, ellig_state_, in->ellig_state_);
+}
+
+void SetHash_Elligator::SetHashImpl::remove_element(const std::string& in)
+{
+    std::array<uint8_t, crypto_core_ed25519_BYTES> p;
+    SetHash_Elligator::SetHashImpl::gen_curve_point(
+        p, reinterpret_cast<const uint8_t*>(in.data()), in.size());
+
+    crypto_core_ed25519_sub(ellig_state_, ellig_state_, p.data());
+}
+
+void SetHash_Elligator::SetHashImpl::remove_set(
+    const SetHash_Elligator::SetHashImpl* in)
+{
+    crypto_core_ed25519_sub(ellig_state_, ellig_state_, in->ellig_state_);
+}
+
+std::array<uint8_t, SetHash_Elligator::kSetHashSize> SetHash_Elligator::
+    SetHashImpl::data() const
+{
+    std::array<uint8_t, kSetHashSize> data;
+    memcpy(data.data(), ellig_state_, kSetHashSize);
+
+    return data;
+}
+
+bool SetHash_Elligator::SetHashImpl::operator==(
+    const SetHash_Elligator::SetHashImpl& h) const
+{
+    return sodium_memcmp(
+               ellig_state_, h.ellig_state_, crypto_core_ed25519_BYTES)
+           == 0;
+}
+
+} // namespace crypto
+} // namespace sse
---
+++ src/set_hash_elligator.hpp
@@ -0,0 +1,191 @@
+//
+// libsse_crypto - An abstraction layer for high level cryptographic features.
+// Copyright (C) 2015-2017 Raphael Bost
+//
+// This file is part of libsse_crypto.
+//
+// libsse_crypto is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// libsse_crypto is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with libsse_crypto.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#pragma once
+
+#include <string>
+#include <vector>
+
+namespace sse {
+
+namespace crypto {
+
+///
+/// @class SetHash
+/// @brief Incremental set hashing
+///
+/// SetHash implements a (multi)set hash function (cf. *Incremental Multiset
+/// Hash Functions and Their Application to Memory Integrity Checking* --
+/// https://people.csail.mit.edu/devadas/pubs/mhashes.pdf ). It allows to
+/// compute the hash of a set of elements, without accounting for the order in
+/// which the elements are hashed. Also, it is easy to compute to the hash of
+/// \f$S \cup \{x\}\f$ given the hash of S (it can be done in constant time,
+/// without having to enumerate all the elements in S). Same thing for the
+/// suppression of \f$x \in S\f$.
+///
+/// This implementation uses the elliptic curve multiset hash (ECMH) by by
+/// Maitin-Shepard, Tibouchi and Aranha (see https://arxiv.org/abs/1601.06502 )
+/// implemented on Ed25519 using libsodium's Elligator primitives introduced in
+/// libsodium 1.0.16
+///
+/// The sets that can be hashed are sets of std::string.
+///
+class SetHash_Elligator
+{
+public:
+    /// @brief Size of the bytes representation of a SetHash
+    static constexpr size_t kSetHashSize = 32;
+    ///
+    /// @brief Constructor
+    ///
+    /// Creates and initializes a SetHash for an empty set.
+    ///
+    SetHash_Elligator();
+
+    ///
+    /// @brief Constructor
+    ///
+    /// Creates a new SetHash from an already computed hash, in its hexadecimal
+    /// representation
+    ///
+    /// @param data A bytes array representing a set hash.
+    ///
+    explicit SetHash_Elligator(const std::array<uint8_t, kSetHashSize>& data);
+
+    ///
+    /// @brief Copy constructor
+    ///
+    SetHash_Elligator(const SetHash_Elligator& o);
+
+    ///
+    /// @brief Move constructor
+    ///
+    SetHash_Elligator(SetHash_Elligator&& o) noexcept;
+
+    ///
+    /// @brief Constructor
+    ///
+    /// Creates a new SetHash representing a vector (list) of strings
+    ///
+    /// @param in_set   The elements to be hashed.
+    ///
+    explicit SetHash_Elligator(const std::vector<std::string>& in_set);
+
+    ///
+    /// @brief Destructor
+    ///
+    ~SetHash_Elligator();
+
+    ///
+    /// @brief Hash a new element in the set hash
+    ///
+    /// Compute the hash of \f$S \cup \{in\} \f$ where S is the set represented
+    /// by the object.
+    ///
+    /// @param in   The element to insert
+    ///
+    void add_element(const std::string& in);
+
+    ///
+    /// @brief Compute the hash of a union
+    ///
+    /// Compute the hash of \f$S \cup S'\f$ where S is the set represented by
+    /// the object, and S' the set represented by h.
+    ///
+    /// @param h    The set hash of the set to insert in the target object
+    ///
+    void add_set(const SetHash_Elligator& h);
+
+
+    ///
+    /// @brief Remove an element of the set hash
+    ///
+    /// Compute the hash of \f$S \setminus \{in\} \f$ where S is the set
+    /// represented by the object.
+    ///
+    /// @param in   The element to insert
+    ///
+    void remove_element(const std::string& in);
+
+    ///
+    /// @brief Compute the hash of a set difference
+    ///
+    /// Compute the hash of \f$S \setminus S'\f$ where S is the set represented
+    /// by the object, and S' the set represented by h.
+    ///
+    /// @param h    The set hash of the set to remove from the target object
+    ///
+    void remove_set(const SetHash_Elligator& h);
+
+    ///
+    /// @brief Binary representation of the SetHash
+    ///
+    /// Returns a bytes array containing the representation of the
+    /// SetHash object.
+    ///
+    /// @return The array representing the set hash
+    ///
+    std::array<uint8_t, kSetHashSize> data() const;
+
+
+    ///
+    /// @brief Stream serialization operator
+    ///
+    /// Put the hex string representation of a SetHash in an output stream.
+    ///
+    /// @param os   The output stream
+    /// @param h    The set hash to serialize in the stream
+    ///
+    /// @return     The stream os
+    ///
+    friend std::ostream& operator<<(std::ostream&            os,
+                                    const SetHash_Elligator& h);
+
+    ///
+    /// @brief Assignment operator
+    ///
+    /// @param h    The element to assign
+    /// @return     The assigned object
+    ///
+    SetHash_Elligator& operator=(const SetHash_Elligator& h);
+
+    ///
+    /// @brief Comparison operator
+    ///
+    /// @param h    The element to compare
+    /// @return     true if h and the object have the same hash, false otherwise
+    ///
+    bool operator==(const SetHash_Elligator& h) const;
+
+    ///
+    /// @brief Comparison operator
+    /// @param h    The element to compare
+    /// @return     false if h and the object have the same hash,
+    ///             true otherwise
+    ///
+    bool operator!=(const SetHash_Elligator& h) const;
+
+private:
+    class SetHashImpl;          // not defined in the header
+    SetHashImpl* set_hash_imp_; // opaque pointer
+};
+
+} // namespace crypto
+} // namespace sse

48ecbc2109ad121272a20c285ce154743660fae1
Raphael Bost <raphael_bost@alumni.brown.edu>
2018-11-01 13:18:22 +0100

    add_compile_definitions is not supported in Travis' version of CMake. Use add_definitions instead.

--- cmake/FindDependencies.cmake
+++ cmake/FindDependencies.cmake
@@ -7,7 +7,7 @@ include(FindOpenSSL)
 if (OPENSSL_FOUND)
     message (STATUS "OpenSSL Include directories:" ${OPENSSL_INCLUDE_DIR})
 
-    add_compile_definitions(WITH_OPENSSL)
+    add_definitions(-DWITH_OPENSSL)
 else() 
     if(RSA_IMPL_OPENSSL)
         message(FATAL_ERROR "OpenSSL's implementation of RSA was chosen and theb OpenSSL library was not found.")

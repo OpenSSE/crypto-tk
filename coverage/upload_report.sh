c6b57c6379fe4daeb5018747cd3ae0d5d22dd660
Raphael Bost <raphael_bost@alumni.brown.edu>
2018-11-01 14:02:11 +0100

    Use the correct location of the coverage.info file when uploading the coverage report

--- coverage/upload_report.sh
+++ coverage/upload_report.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 set -ex
 
-lcov --list build/check_coverage/check_coverage.info # debug before upload
-coveralls-lcov build/check_coverage/coverage.info
+lcov --list build/check_coverage.info # debug before upload
+coveralls-lcov build/coverage.info

64a76cdcdf0b30d026c7668a7598c4c81377651d
Raphael Bost <raphael_bost@alumni.brown.edu>
2018-11-03 17:51:17 +0100

    Also use the build commands for ccpcheck

--- scripts/cppcheck.sh
+++ scripts/cppcheck.sh
@@ -1,11 +1,25 @@
 #! /bin/bash
+set -e
+
 if [[ -z $CPPCHECK ]]; then
 	CPPCHECK="cppcheck"
 fi
 
-INCLUDES="-Ibuild -Isrc -I/usr/local/opt/openssl/include -Isrc/include -Isrc/include/opensse/crypto"
+echo "Using "$CPPCHECK
 
-COMMAND="$CPPCHECK src -i src/mbedtls $INCLUDES --quiet --verbose --std=c++11 --force  --enable=warning,performance,portability,style --error-exitcode=1 --report-progress  --inline-suppr --xml"
+echo "Generate the compile commands"
+
+mkdir -p build
+cd build
+# For the static analysis, only focus on an AES NI-enabled target
+CFLAGS="-maes -DWITH_OPENSSL" CXXFLAGS="-maes -DWITH_OPENSSL" cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ../src
+cd ..
+
+set +e
+
+CPPCHECK_TEMPLATE="gcc"
+
+COMMAND="$CPPCHECK --project=build/compile_commands.json -i src/mbedtls --std=c++11 --force  --enable=warning,performance,portability,style --error-exitcode=1 --report-progress  --inline-suppr --template=$CPPCHECK_TEMPLATE"
 
 echo $COMMAND
 

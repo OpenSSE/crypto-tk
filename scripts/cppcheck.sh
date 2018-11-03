d9803541de1d8c39ff58c61d71ba06bfe127ab12
Raphael Bost <raphael_bost@alumni.brown.edu>
2018-11-04 00:33:53 +0100

    Due to cppcheck compatibility issue and limitations, go back to the previous behavior of the cppcheck script (namely not using the build commands).

--- scripts/cppcheck.sh
+++ scripts/cppcheck.sh
@@ -8,23 +8,35 @@ bold=$(tput bold)
 normal=$(tput sgr0)
 red=$(tput setaf 1)
 
-echo "Using $CPPCHECK"
-
-if [ ! -f $STATIC_ANALYSIS_DIR/compile_commands.json ]; then
-    echo "Generate the compile commands"
-
-    mkdir -p $STATIC_ANALYSIS_DIR 
-    cd $STATIC_ANALYSIS_DIR
-    # For the static analysis, only focus on an AES NI-enabled target
-    CFLAGS="-maes -DWITH_OPENSSL" CXXFLAGS="-maes -DWITH_OPENSSL" cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ../src
-    cd ..
-fi
-
-set +e
-
 CPPCHECK_TEMPLATE="${bold}{file}:{line}${normal}\\n${bold}${red}error: ${normal}${bold}{severity}({id}): {message}${normal}\\n{code}"
 
-COMMAND="$CPPCHECK --project=$STATIC_ANALYSIS_DIR/compile_commands.json -i src/mbedtls --std=c++11 --force  --enable=warning,performance,portability,style --error-exitcode=1 --report-progress  --inline-suppr --template='$CPPCHECK_TEMPLATE'"
+CPPCHECK_OPTIONS="-i src/mbedtls --std=c++11 --force  --enable=warning,performance,portability,style --error-exitcode=1 --report-progress  --inline-suppr --template='$CPPCHECK_TEMPLATE'"
+
+echo "Using $CPPCHECK"
+
+# legacy_cppcheck=true
+
+# if [ "$legacy_cppcheck" = true ] ; then
+#     INCLUDES="-Ibuild -Isrc -I/usr/local/opt/openssl/include -Isrc/include -Isrc/include/opensse/crypto"
+
+    COMMAND="$CPPCHECK src $INCLUDES $CPPCHECK_OPTIONS"
+
+# else
+#     if [ ! -f $STATIC_ANALYSIS_DIR/compile_commands.json ]; then
+#         echo "Generate the compile commands"
+
+#         mkdir -p $STATIC_ANALYSIS_DIR 
+#         cd $STATIC_ANALYSIS_DIR
+#         # For the static analysis, only focus on an AES NI-enabled target
+#         CFLAGS="-maes -DWITH_OPENSSL" CXXFLAGS="-maes -DWITH_OPENSSL" cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ../src
+#         cd ..
+#     fi
+
+
+#     COMMAND="$CPPCHECK --project=$STATIC_ANALYSIS_DIR/compile_commands.json $CPPCHECK_OPTIONS"
+# fi
+
+set +e
 
 echo $COMMAND
 
